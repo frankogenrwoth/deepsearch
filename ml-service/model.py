49412be383db0dfcf2b94e8a0bc03cc60a4a3ae4
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-01-18 23:57:36 +0300

    add load embedor

--- ml-service/model.py
+++ ml-service/model.py
@@ -1,14 +1,20 @@
+from pathlib import Path
 from transformers import AutoTokenizer, AutoModel
 
 from sentence_transformers import SentenceTransformer
 
-MODEL_SAVE_DIR = "ml-service\\models\\sentence-transformers"
+MODEL_SAVE_DIR = Path("ml-service\\models\\sentence-transformers").absolute()
 
 class Model:
   """A model class to lead the model and tokenizer"""
 
   def __init__(self) -> None:
     pass
+
+  def load_embeddor():
+    embeddor = SentenceTransformer(MODEL_SAVE_DIR)
+    return embeddor
+
   
   def load_model(with_sentence_transformer=False):
     if with_sentence_transformer:
