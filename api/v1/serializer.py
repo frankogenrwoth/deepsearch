3bf39214b8b9630603a8b5a9ca4f22f9f3afe564
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-01-18 10:18:48 +0300

    add search result serializer

--- api/v1/serializer.py
+++ api/v1/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Collaborator
+from api.models import Collaborator, Project
 
 class CollaboratorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,5 +7,17 @@ class CollaboratorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ProjectSerializer(serializers.ModelSerializer):
+    collaborators = CollaboratorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+        exclude = ('embedding',)
 
 
+class SearchResultsSerializer(serializers.Serializer):
+    match_id = serializers.IntegerField()
+    data = serializers.CharField()
+    key = serializers.CharField()
+    date_added = serializers.DateTimeField()
